8e904210ac798366c5c6669ce2f117341b0c0ab7
Roman Troyan <troyan.roman1985@gmail.com>
2023-10-12 23:25:53 +0300

    View resources added. Login fragment started

--- Android/YourDrive/app/src/main/java/com/example/yourdrive/sl/MainModule.kt
+++ Android/YourDrive/app/src/main/java/com/example/yourdrive/sl/MainModule.kt
@@ -1,15 +1,17 @@
 package com.example.yourdrive.sl
 
 import com.example.yourdrive.MainViewModel
+import com.github.johnnysc.coremvvm.presentation.CanGoBack
 import com.github.johnnysc.coremvvm.presentation.NavigationCommunication
 import com.github.johnnysc.coremvvm.sl.CoreModule
 import com.github.johnnysc.coremvvm.sl.Module
 
-class MainModule(private val coreModule: CoreModule) : Module<MainViewModel.Base> {
-    override fun viewModel(): MainViewModel.Base = MainViewModel.Base(
-        NavigationCommunication.Base(),
-        coreModule.provideProgressCommunication(),
-        coreModule.dispatchers(),
-        coreModule.provideGlobalErrorCommunication()
+class MainModule(private val coreModule: CoreModule) : Module<MainViewModel> {
+    override fun viewModel(): MainViewModel = MainViewModel(
+        navigationCommunication = NavigationCommunication.Base(),
+        progressCommunication = coreModule.provideProgressCommunication(),
+        dispatchers = coreModule.dispatchers(),
+        communication = coreModule.provideGlobalErrorCommunication(),
+        canGoBack = CanGoBack.Empty()
     )
 }
